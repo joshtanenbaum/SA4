ea277386d8577fe96f6a62f1789ff4f2d5ed5220
Josh Tanenbaum <Joshua.D.Tanenbaum.20@dartmouth.edu>
2020-04-18 11:02:41 -0400

    done

--- src/index.js
+++ src/index.js
@@ -1,18 +1,57 @@
 // change require to es6 import style
-import $ from 'jquery';
 import './style.scss';
-import React from 'react';
+import debounce from 'lodash.debounce';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import youtubeSearch from './youtube-api';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-function p(x) {
+
+/* function p(x) {
   const num = (new Date().getTime()) - x;
   $('#main').html(`Youve been on this page for ${num / 1000} seconds.`);
 }
 
 const starttime = new Date().getTime();
-setInterval(() => { p(starttime); }, 1000);
+setInterval(function() { p(starttime); }, 1000);  */
 
 
-const App = () => <div className="test">All the REACT are belong to us!</div>;
+class App extends Component {
+  // here's what our constructor would look like
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
+
+    this.search = debounce(this.search, 300);
+    this.search('pixar');
+  }
+
+  search = (text) => {
+    youtubeSearch(text).then((videos) => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0],
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar onSearchChange={this.search} />
+        <div id="video-section">
+          <VideoDetail video={this.state.selectedVideo} />
+          <VideoList onVideoSelect={(selectedVideo) => this.setState({ selectedVideo })} videos={this.state.videos} />
+        </div>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('main'));
